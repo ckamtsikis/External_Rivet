efcb6e822b6538bb0f44287447f13214dce158fd
Christos <ckamtsikis@gmail.com>
2021-06-24 15:28:32 +0200

    Paths

--- JetAnalysis/test/exe.sh
+++ JetAnalysis/test/exe.sh
@@ -2,6 +2,6 @@
 
 cd afspath                                   # to be set from run_condor.sh script
 eval `scramv1 runtime -sh`                   # this is the cmsenv
-source $CMSSW_BASE/src/External_Rivet3/JetAnalysis/test/source_rivetSetup.sh
+source $CMSSW_BASE/src/External_Rivet/JetAnalysis/test/source_rivetSetup.sh
 
 cmsRun cfg.py                                # to be set from run_condor.sh script
