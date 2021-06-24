64808e7a5a185b23a03fb73e5da7c679f942cf50
Christos Kamtsikis <40173921+ckamtsikis@users.noreply.github.com>
2021-06-24 16:36:05 +0300

    Update source_rivetSetup.sh

--- JetAnalysis/test/source_rivetSetup.sh
+++ JetAnalysis/test/source_rivetSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source $CMSSW_BASE/src/Rivet/rivetSetup.sh
 source $CMSSW_BASE/src/GeneratorInterface/RivetInterface/test/rivetSetup.sh
-export RIVET_REF_PATH=$RIVET_REF_PATH:$CMSSW_BASE/src/External_Rivet3/JetAnalysis/data
-export RIVET_INFO_PATH=$RIVET_INFO_PATH:$CMSSW_BASE/src/External_Rivet3/JetAnalysis/data
-export RIVET_PLOT_PATH=$RIVET_PLOT_PATH:$CMSSW_BASE/src/External_Rivet3/JetAnalysis/data
+export RIVET_REF_PATH=$RIVET_REF_PATH:$CMSSW_BASE/src/External_Rivet/JetAnalysis/data
+export RIVET_INFO_PATH=$RIVET_INFO_PATH:$CMSSW_BASE/src/External_Rivet/JetAnalysis/data
+export RIVET_PLOT_PATH=$RIVET_PLOT_PATH:$CMSSW_BASE/src/External_Rivet/JetAnalysis/data
 
